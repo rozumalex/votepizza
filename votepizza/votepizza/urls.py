eea7ee1eaee2d11b48dbb7c76bf48637fdf39b27
Alex Rozum <rozumalex@gmail.com>
2020-10-13 02:30:44 +0300

    Update swagger pattern

--- votepizza/votepizza/urls.py
+++ votepizza/votepizza/urls.py
@@ -4,11 +4,14 @@ from django.conf.urls import include
 from rest_framework_swagger.views import get_swagger_view
 
 
-schema_view = get_swagger_view(title='Pastebin API')
+swagger_pattern = [
+    path('api/', include('pizza.urls', namespace='pizza')),
+]
 
+schema_view = get_swagger_view(title='Pastebin API', patterns=swagger_pattern)
 
 urlpatterns = [
-    path('', schema_view),
+    path('', schema_view, name='site_root'),
     path('api/', include('pizza.urls', namespace='pizza')),
     path('admin/', admin.site.urls),
 ]
